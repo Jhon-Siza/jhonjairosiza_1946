fe7d8392166860a527af81fa77ada6b856f08bc3
jhon siza <jhonsiza@hotmail.com>
2025-02-05 11:00:57 -0500

    creacion de librerias arrays

--- arrays/busqueda/busqueda_binaria.cpp
+++ arrays/busqueda/busqueda_binaria.cpp
@@ -1,76 +1,39 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include "../ordenamiento/insercion.cpp"
+#include "../../librerias/arrays.h"
 using namespace std;
-void llenaDatos(int v[],int n)
-{
-     srand(time(NULL));
-    for(int i=0; i<n; i++){
-        //v[i]=rand()%20+1;
-        cin>>v[i];
-    }
+using namespace vectorn;
 
-}
-void verDatos(int v[],int n)
-{
-    for(int i=0; i<n; i++){
-        cout<<v[i]<<"\t";
-    }
-
-}
-int ordenaBusqueda(int v[],int ne){
-    int contar=0,aux;
-    for(int i=1; i<ne;i++){
-        aux=v[i];
-        int j = i-1;
-        while (j>=0){
-            contar++;
-            if(aux <v[j]){
-                v[j+1]=v[j];
-                v[j]=aux;
-            }
-            j--;
+bool isBusquedaBinaria(int v[], int n, int elemento){
+    int arriba, abajo, centro;
+    abajo = 0;
+    arriba = n-1;
+    while (abajo<=arriba){
+        centro = (abajo+arriba)/2;
+        if(v[centro]==elemento){
+            return true;
+        }else if(v[centro]<elemento){
+            abajo = centro +1;
+        }else{
+            arriba = centro-1;
         }
     }
-    return contar;
-
-
+    return false;
 }
 
-bool isBusquedaBinaria(int v[],int n, int elemento)
-{
-    int Iabajo,Iarriba, Icentro;
-    Iabajo=0;
-    Iarriba=n-1;
-    Icentro=(Iabajo+Iarriba)/2;
-    while(Iabajo<=Iarriba && v[Icentro]!=elemento)
-    {
-        if(v[Icentro]<elemento)
-        {
-            Iabajo=Icentro+1;
-        }
-        else
-        {
-            Iarriba=Icentro-1;
-        }
-        Icentro=(Iabajo+Iarriba)/2;
-    }
-    return (Iabajo<=Iarriba);
-
-}
-
-main()
-{
-    int ne,dato;
-    cout<<"Nro de elementos del array: ";
-    cin>>ne;
+main(){
+    int ne, dato;
+    cout << "Nro de Elementos del Array: ";
+    cin >> ne;
     int vector[ne];
-    llenaDatos(vector,ne);
-    cout<<"Ingrese el dato a buscar: ";
-    cin>>dato;
-    ordenaInsercion(vector,ne);
-    (isBusquedaBinaria(vector,ne,dato))?cout<<"Datos encontrado ":cout<<"Datos no encontrado ";
-    return 0;
-
+    llenarVector(vector, ne,20,50);
+    cout << "Datos originales\n";
+    verVector(vector, ne);
+    cout<<"\nDatos ordenados\n";
+    ordenaBurbujav3(vector, ne);
+    verVector(vector, ne);
+    cout<<"\nIngrese el dato a buscar: ";
+    cin >> dato;
+    (isBusquedaBinaria(vector, ne, dato))?cout << "Dato Encontrado":cout<<"Dato no encontrado";
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,58 @@
-#include "factorial.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "../../librerias/arrays.h"
 
-// Definición de la función factorial
-int calcularFactorial(int n) {
-    if(n == 0 || n == 1) {
-        return 1;
-    } else {
-        return n * calcularFactorial(n - 1);
+using namespace std;
+
+void verDatos(int v[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << "\t";
     }
+    cout << endl;
+}
+
+bool isBusquedaBinaria(int v[], int n, int elemento) {
+    int Iabajo = 0, Iarriba = n - 1, Icentro;
+    
+    while (Iabajo <= Iarriba) {
+        Icentro = Iabajo + (Iarriba - Iabajo) / 2;
+        
+        if (v[Icentro] == elemento) {
+            return true;
+        } else if (v[Icentro] < elemento) {
+            Iabajo = Icentro + 1;
+        } else {
+            Iarriba = Icentro - 1;
+        }
+    }
+    return false;
+}
+
+int main() {
+    srand(time(0));
+    int ne = 10, dato;
+    int vector[ne];
+    
+    llenarVector(vector, ne, 0, 10);
+    ordenaBurbujav3(vector, ne);
+    
+    cout << "Bienvenido al juego de adivinar el número!" << endl;
+    cout << "Intenta adivinar un número entre 0 y 10." << endl;
+    
+    verDatos(vector, ne);
+    
+    while (true) {
+        cout << "\nIngrese su suposición: ";
+        cin >> dato;
+        
+        if (isBusquedaBinaria(vector, ne, dato)) {
+            cout << "¡Felicidades! Has adivinado un número dentro del rango." << endl;
+            break;
+        } else {
+            cout << "Número incorrecto, intenta nuevamente." << endl;
+        }
+    }
+    
+    return 0;
 }
