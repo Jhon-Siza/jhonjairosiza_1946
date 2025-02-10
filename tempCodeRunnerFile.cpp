1338a8734c5efa8f098bebbbe225919b6559997f
jhon siza <jhonsiza@hotmail.com>
2025-02-10 11:01:54 -0500

    funciones_cadenas10_feb

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,58 +1,10 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include "../../librerias/arrays.h"
-
 using namespace std;
-
-void verDatos(int v[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << v[i] << "\t";
-    }
-    cout << endl;
-}
-
-bool isBusquedaBinaria(int v[], int n, int elemento) {
-    int Iabajo = 0, Iarriba = n - 1, Icentro;
-    
-    while (Iabajo <= Iarriba) {
-        Icentro = Iabajo + (Iarriba - Iabajo) / 2;
-        
-        if (v[Icentro] == elemento) {
-            return true;
-        } else if (v[Icentro] < elemento) {
-            Iabajo = Icentro + 1;
-        } else {
-            Iarriba = Icentro - 1;
-        }
-    }
-    return false;
-}
-
-int main() {
-    srand(time(0));
-    int ne = 10, dato;
-    int vector[ne];
-    
-    llenarVector(vector, ne, 0, 10);
-    ordenaBurbujav3(vector, ne);
-    
-    cout << "Bienvenido al juego de adivinar el número!" << endl;
-    cout << "Intenta adivinar un número entre 0 y 10." << endl;
-    
-    verDatos(vector, ne);
-    
-    while (true) {
-        cout << "\nIngrese su suposición: ";
-        cin >> dato;
-        
-        if (isBusquedaBinaria(vector, ne, dato)) {
-            cout << "¡Felicidades! Has adivinado un número dentro del rango." << endl;
-            break;
-        } else {
-            cout << "Número incorrecto, intenta nuevamente." << endl;
-        }
-    }
-    
-    return 0;
+main()
+{
+    int a,b;
+    cout<<"cfm valito";
+    cin>>a;
+    cout<<"que hora";
+    cin>>b;
 }
