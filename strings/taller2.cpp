63442afaa0be4b2831b32bd23653afaa6f348779
jhon siza <jhonsiza@hotmail.com>
2025-02-12 11:00:56 -0500

    taller strings

---
+++ strings/taller2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    string cadena1 = {"informatica"};
+    string cadena4;
+    const char *auxiliar;
+    string cadena2(cadena1);//cadena2 = cadena1
+    string cadena3(10, '*');
+    cadena1.push_back('Z');//permite añadir 1 caracter al final
+    cout<<cadena1<<endl;
+    cadena1.at(0)= 'I';//metodo at accede a un indice de la cadena cadena1[0]
+    cout<<cadena1<<endl;
+    //usando el metodo front y back para cambiar los caracteres inicio y final
+    cadena1.front()='i';
+    cadena1.back()='A';
+    cout<<cadena1<<endl;
+    auxiliar = cadena1.c_str();
+    cout<<"auxiliar array char: "<<auxiliar<<endl;
+    //metodo empty para conocer si una cadena esta vacia o no
+    (cadena4.empty())?cout<<"Cadena vacia":cout<<"cadena no vacia\n";
+    //tamaño maximo de caracteres que puede almacenar la cadena
+    cout << "Tamano maximo: " << cadena1.max_size() << endl;
+    //tamaño en bytes del objeto string que ocupa actualemnte
+    cout << "Capacidad: " << cadena1.capacity()<<endl;
+
+
+
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-main()
-{
-    int a,b;
-    cout<<"cfm valito";
-    cin>>a;
-    cout<<"que hora";
-    cin>>b;
+main(){
+int a, b;
+cout<< "ingresa el primer numero: ";
+cin>> a;
+cout<< "ingresa el segundo numero: ";
+cin>> b;
+cout<< "A mas "<<a<<" y "<<b<<" es: "<<a+b<<endl;
 }
+
